1fa3da18b2190bf66401a4dc21dde069247a606d
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2021-03-31 11:36:19 +0100

    Added def for the most frequent bigram
    Print most Freq bigram

--- tokenizor.py
+++ tokenizor.py
@@ -25,3 +25,18 @@ for item in bigrams_2:
         my_dict_il[item]=1 #add to dictionary and make count 1
 
 #print(my_dict_il)
+
+def top_word_BOW(n):
+    bow_list = [] #making the dictionary a list so create a blank list
+    for key, value in n.items(): #for every key/ value in the dictionary..
+        vk = (value, key) #store each value and key in opposite order in a variable
+        bow_list.append(vk) #write each value and key to the new list
+
+    bow_list = sorted(bow_list) #sort the list
+    #bow_list = sorted(bow_list, reverse=True) #reverse sort
+    top_word = bow_list[-1] #create varviable from the last item in the list (which is the now highest dict item)
+    top_word = list(top_word) #make that pair a list type
+    print ("The most frequent word occuring in the text is:             ", top_word[1]) #print the second item in the ist (i.e., the word)
+    print ("The most frequent word occurs the following number of times:", top_word[0])
+    
+top_word_BOW(my_dict_il) #call the function
