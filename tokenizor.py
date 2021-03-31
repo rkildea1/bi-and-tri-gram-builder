7b649955c93e3d8b32c4db9a07c0a48774d598be
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2021-03-31 11:20:39 +0100

    Vectorization
    Added a dictionary counter to the bigrams

--- tokenizor.py
+++ tokenizor.py
@@ -9,4 +9,19 @@ def bigram(file,n=2,i=0):
         i += 1
 bigrams_lists = list(bigram(q3_words_list, n=2)) #this is my bigram list. list inside a list
 
-print(bigrams_lists)
+#print(bigrams_lists)
+
+bigrams_2 = []
+for il in bigrams_lists: #for inner list in 
+    bigrams_2.append((il[0],il[1]))
+#print(bigrams_2)
+
+my_dict_il = {} #create a blank dictionary to write the word list to
+
+for item in bigrams_2:
+    if item in my_dict_il.keys(): #if the word in the wordlist is already a key in the dictionary then...
+        my_dict_il[item]+=1 #increase the count by one
+    else:
+        my_dict_il[item]=1 #add to dictionary and make count 1
+
+#print(my_dict_il)
